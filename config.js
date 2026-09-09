e401ddacedec36d141889182dd23a3a96ee00d17
kyawkolatt2026-alt <kyawkolatt2026@gmail.com>
2026-09-10 02:16:47 +0700

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const SUPABASE_URL = "https://oajknztsxshrmzryhslk.supabase.co";
-const SUPABASE_KEY = "sb_publishable_C1oevhHeuMmyJpfPHCAnWg_jtfw95oS";
-const YOUTUBE_API_KEY = "AIzaSyBMTYuL3d_4I9UqPAqatwHP1OYOa6lB7nQ";
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
