f2e67f4f7a745cd94140e0796675feca74b0b1d8
kyawkolatt2026-alt <kyawkolatt2026@gmail.com>
2026-09-09 18:00:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "YOUR_SUPABASE_PROJECT_URL";
+const SUPABASE_KEY = "YOUR_SUPABASE_PUBLISHABLE_KEY";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
